2794b31ae47560092a57fff205f256e9fdb1f340
Doan Phu Sanh <dpsanh2348@gmail.com>
2019-04-16 07:28:57 +0700

    update bonus features

---
+++ README.md
@@ -0,0 +1,36 @@
+# CoderSchool Golang Course Prework Submission Template
+
+1. **Submitted by:**
+2. **Time spent:**
+
+## Set of User Stories
+
+### Required
+* [x] Command-line argument parsing
+* [x] Input params
+   * [x] Requests - Number of requests to perform
+   * [x] Concurrency - Number of multiple requests to make at a time
+   * [x] URL - The URL for testing.
+* [x] Prints use information if wrong arguments provided
+* [x] Implements  HTTP load and summarize it
+* [x] Concurrency must be implemented with goroutine.
+
+
+### Bonus
+* [x] Extend input params with: 
+   * [x] Timeout - Seconds to max. wait for each response
+   * [x] Timelimit - Maximum number of seconds to spend for benchmarking
+* [ ] Prints key metrics of summary, such:
+   * [x] Server Hostname
+   * [ ] Server Port
+   * [ ] Document Path
+   * [ ] Document Length
+   * [ ] Concurrency Level
+   * [ ] Time taken for tests
+   * [x] Complete requests
+   * [x] Failed requests
+   * [ ] Total transferred
+   * [ ] Requests per second
+   * [x] Time per request
+   * [x] Time per request
+   * [ ] Transfer rate
--- mybench.go
+++ mybench.go
@@ -14,15 +14,19 @@ type responseInfo struct {
 	status int
 	bytes int64
 	duration time.Duration
+	ServerHostname string
+	TimePerRequest time.Duration
+	CompleteRequest int64
+	FailedRequest int64
 }
 
 type summaryInfo struct {
 	requested int64
 	responded int64
-}
-
-type ResponseBody struct {
-	ServerHostname string `json:"server_hostname"`
+	ServerHostname string
+	TimePerRequest time.Duration
+	CompleteRequest int64
+	FailedRequest int64
 }
 
 func main() {
@@ -30,8 +34,11 @@ func main() {
 
 	requests := flag.Int64("n", 1, "Number of requests to perform")
 	concurrency := flag.Int64("c", 1, "Number of multiple requests to make at a time")
-	timeout := flag.Int64("s", 1, "Seconds to max. wait for each response. Default is 30 seconds")
-	timelimit := flag.Int64("t", 1, "Seconds to max. to spend on benchmarking. This implies -n 50000")
+	s := flag.Int64("s", 30, "Seconds to max. wait for each response. Default is 30 seconds")
+	t := flag.Int64("t", 30, "Seconds to max. to spend on benchmarking. This implies -n 50000")
+
+	timeout := time.Duration(*s) * time.Millisecond
+	timelimit := time.Duration(*t) * time.Millisecond
 
 	fmt.Println(requests, concurrency, timeout, timelimit)
 
@@ -43,44 +50,71 @@ func main() {
 		os.Exit(-1)
 	}
 
+	var timePerRequest []time.Duration
 	link := flag.Arg(0)
 
 	c := make(chan responseInfo)
 	summary := summaryInfo{}
 	for i := int64(0); i < *concurrency; i++ {
 		summary.requested++
-		go checkLink(link, c)
+		go checkLink(link, timeout, timelimit, c)
 	}
 
 	for response := range c {
 		if summary.requested < *requests {
 			summary.requested++
-			go checkLink(link, c)
+			go checkLink(link, timeout, timelimit, c)
 		}
 		summary.responded++
+		summary.CompleteRequest++
 		fmt.Println(response)
+		summary.ServerHostname = response.ServerHostname
+		timePerRequest = append(timePerRequest, response.TimePerRequest)
 		if summary.requested == summary.responded {
 			break
 		}
 	}
+
+	var timePerRequestAverage float64
+	for time := range timePerRequest {
+		timePerRequestAverage += float64(time)
+	}
+	timePerRequestAverage = timePerRequestAverage / float64(len(timePerRequest))
+
+	summary.FailedRequest = *requests - summary.CompleteRequest
+	fmt.Println("--- summary ---")
+	fmt.Println("+ Server Hostname: ", summary.ServerHostname)
+	fmt.Println("+ Time Per Request: ", timePerRequestAverage)
+	fmt.Println("+ Complete Request: ", summary.CompleteRequest)
+	fmt.Println("+ Failed Request: ", summary.FailedRequest)
+
 }
 
-func checkLink(link string, c chan responseInfo) {
+func checkLink(link string, timeout time.Duration, timelimit time.Duration, c chan responseInfo) {
 	start := time.Now()
-	res, err := http.Get(link)
+	//res, err := http.Get(link)
+
+	client := http.Client{}
+	request, _ := http.NewRequest("GET", link, nil)
+	resp, err := client.Do(request)
+
 	if err != nil {
 		panic(err)
 	}
 
-	read, _ := io.Copy(ioutil.Discard, res.Body)
+	defer resp.Body.Close()
+
+	read, _ := io.Copy(ioutil.Discard, resp.Body)
 
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
 	c <- responseInfo{
-		status: res.StatusCode,
+		status: resp.StatusCode,
 		bytes: read,
 		duration: time.Now().Sub(start),
+		ServerHostname: request.Host,
+		TimePerRequest: time.Now().Sub(start),
 	}
 }
